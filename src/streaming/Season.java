58efc54fdfcb13d837e06703ebc9ec935a730d7f
Madiyar Tankayev <madiyar.tankayev@narxoz.kz>
2025-05-03 00:00:52 +0500

    feat: added Season

---
+++ src/streaming/Season.java
@@ -0,0 +1,22 @@
+package streaming;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Season implements Iterable<Episode> {
+    private final List<Episode> episodes = new ArrayList<>();
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    @Override
+    public Iterator<Episode> iterator() {
+        return new SeasonIterator(episodes);
+    }
+}
